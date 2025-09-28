4def124f02adfc95119c2c069ac1d59a9f87d21e
Fasteroid <29342750+Fasteroid@users.noreply.github.com>
2025-09-28 04:25:38 -0400

    conclusion: berserkium is GROSS

--- files/entities/window_berserk.lua
+++ files/entities/window_berserk.lua
@@ -3,5 +3,23 @@ dofile_once("mods/windows/files/window_common.lua")
 local crossing_projectiles = FindCrossingProjectiles("crossed_window_berserk")
 
 for i=1, #crossing_projectiles do
-    print( tostring(crossing_projectiles[i]), "went through a berserk window" )
+    local proj = crossing_projectiles[i]
+
+    local children = EntityGetAllComponents(proj)
+
+    for j=1, #children do
+        local child = children[j]
+        local kind = ComponentGetTypeName(child)
+        
+
+        local members = ComponentGetMembers(child)
+        for k, v in pairs(members) do
+            if( v == nil ) then goto continue_2 end
+            print(kind, k, v)
+            ::continue_2::
+        end
+
+    end
+
+    ::continue_1::
 end
